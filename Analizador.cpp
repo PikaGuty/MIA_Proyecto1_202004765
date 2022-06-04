7b1407980fb00b29b33ea43c778849e89dbc4ceb
PikaGuty <3024021520101@ingenieria.usac.edu.gt>
2022-06-04 14:25:23 -0600

    Comando mkdisk bien

--- Analizador.cpp
+++ Analizador.cpp
@@ -5,6 +5,7 @@
 #include "Analizador.h"
 #include <iostream>
 #include <string.h>
+#include <string>
 
 
 
@@ -32,7 +33,7 @@ void analizar(){
         transform(actual.begin(), actual.end(), actual.begin(), ::tolower);
 
         if (actual=="mkdisk"){
-            cout<<"Efectivamente es mkdisk"<<endl;
+            cout<<"******** Ejecutando MKDISK ********"<<endl;
             token = strtok(NULL, " ");
             comando_MKDISK(token);
         }
@@ -48,7 +49,7 @@ void comando_MKDISK(char *token){
 
     string comandos = "";
     comandos+=token;
-    while (token != NULL) {
+    while (token != NULL) { //Convirtiendo los tokens recibidos en una sola cadena
         token = strtok(NULL, " ");
         if (token != NULL){
             comandos+=" ";
@@ -56,56 +57,83 @@ void comando_MKDISK(char *token){
         }
     }
 
-    string ncomando;
+    string ncomando, anterior;
 
-    for (int i = 0; i < 3; ++i) {
+    while (comandos != "") { //Volviendo a desarmar la cadena para obtener los parámetros
         ncomando = comandos.substr(0, comandos.find("=>"));
         transform(ncomando.begin(), ncomando.end(), ncomando.begin(), ::tolower);
         int pos = comandos.find("=>");
-        if (ncomando == "$size" && xsize == false) {
-            xsize = true;
+
+        if (ncomando == "$size" && xsize == false) { //Reconociendo el size y sacando su parámetro
+            xsize = true;//Indicando que ya se evaluó al ser de caracter obligatorio
             comandos.erase(0, 2 + pos);
-            cout << "SIZE ES " << comandos.substr(0, comandos.find(" ")) << endl;
+
+            //PASANDO A LA VARIABLE SIZE EL DATO
+            size = stoi(comandos.substr(0, comandos.find(" ")));
+
             pos = comandos.find(" ");
             comandos.erase(0, 1 + pos);
         } else if (ncomando == "$path" && xpath == false) {
-            xpath = true;
+            xpath = true;//Indicando que ya se evaluó al ser de caracter obligatorio
             comandos.erase(0, 2 + pos);
-            cout << "PATH ES " << comandos.substr(0, comandos.find(" ")) << endl;
+
+            pos = comandos.find("\"");
+            comandos.erase(0, 1 + pos);
+
+            //PASANDO A LA VARIABLE PATH EL DATO
+            strcpy(path, comandos.substr(0, comandos.find("\"")).c_str());
+
+            pos = comandos.find("\"");
+            comandos.erase(0, 1 + pos);
+
             pos = comandos.find(" ");
             comandos.erase(0, 1 + pos);
         } else if (ncomando == "$name" && xname == false) {
-            xname = true;
+            xname = true;//Indicando que ya se evaluó al ser de caracter obligatorio
             comandos.erase(0, 2 + pos);
-            cout << "Name ES " << comandos.substr(0, comandos.find(" ")) << endl;
-            pos = comandos.find("\n");
-            comandos.erase(0, 1 + pos);
 
+            //PASANDO A LA VARIABLE NAME EL DATO
+            strcpy(name,comandos.substr(0, comandos.find(" ")).c_str());
+
+            pos = comandos.find(" ");
+            comandos.erase(0, 1 + pos);
+        }
+
+        if (comandos==anterior){ //Verificando si ya se llegó al final (no cambia por que no encuentra espacio)
+            comandos="";
+        }else{
+            anterior=comandos;
         }
     }
 
-    cout<<"Al terminar tenermos "<<comandos<<endl;
 
-    /*while (token != NULL) {
-        string comando="";
-        comando += token;
-
-        string ncomando = comando.substr(0, comando.find("=>"));
-        transform(ncomando.begin(), ncomando.end(), ncomando.begin(), ::tolower);
-
-        int pos = comando.find("=>");
-        if(ncomando == "$size" && xsize == false){
-            comando.erase(0, 2+pos);
-            cout<<"SIZE ES "<< comando.substr(0, comando.find("=>"))<<endl;
-        }else if(ncomando == "$path" && xpath == false){
-            comando.erase(0, 2+pos);
-            cout<<"PATH ES "<< comando.substr(0, comando.find("=>"))<<endl;
-        }else if(ncomando == "$name" && xname == false){
-            comando.erase(0, 2+pos);
-            cout<<"Name ES "<< comando.substr(0, comando.find("=>"))<<endl;
+    if(xsize == true && xname == true && xpath==true){//Se puede ejecutar el comando
+        //LLAMAR AL METODO PARA TERMINAR VERIFICACIONES Y REALIZAR LO QUE TIENE QUE HACER
+        cout<<"Size: "<<size<<endl;
+        cout<<"Name: "<<name<<endl;
+        cout<<"Path: "<<path<<endl;
+    }else{ //Notificando errores si no se ingresaron los parametros obligatorios al comando
+        cout<<"El comando \"MKDISK\" debe poseer el/los parámetros ";
+        if(xsize == false){
+            cout<<"$size";
+            if(xname == false || xpath == false){
+                cout<<", ";
+            }else{
+                cout<<" ";
+            }
         }
-        cout<<"COMANDO "<<token<<endl;
+        if(xname == false){
+            cout<<"$name";
+            if(xpath == false){
+                cout<<", ";
+            }else{
+                cout<<" ";
+            }
+        }
+        if(xpath == false){
+            cout<<"$path ";
+        }
 
-        token = strtok(NULL, " ");
-    }*/
+        cout<<"ya que son de caracter obligatorio."<<endl;
+    }
 }
