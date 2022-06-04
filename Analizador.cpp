4f9a699c498885dbe18b4b07b445fe39176ba177
PikaGuty <3024021520101@ingenieria.usac.edu.gt>
2022-06-04 09:12:21 -0600

    antes de ir al dentista

--- Analizador.cpp
+++ Analizador.cpp
@@ -46,11 +46,51 @@ void comando_MKDISK(char *token){
     int size;
     char path[512], name[64];
 
+    string comandos = "";
+    comandos+=token;
     while (token != NULL) {
+        token = strtok(NULL, " ");
+        if (token != NULL){
+            comandos+=" ";
+            comandos+=token;
+        }
+    }
+
+    string ncomando;
+
+    for (int i = 0; i < 3; ++i) {
+        ncomando = comandos.substr(0, comandos.find("=>"));
+        transform(ncomando.begin(), ncomando.end(), ncomando.begin(), ::tolower);
+        int pos = comandos.find("=>");
+        if (ncomando == "$size" && xsize == false) {
+            xsize = true;
+            comandos.erase(0, 2 + pos);
+            cout << "SIZE ES " << comandos.substr(0, comandos.find(" ")) << endl;
+            pos = comandos.find(" ");
+            comandos.erase(0, 1 + pos);
+        } else if (ncomando == "$path" && xpath == false) {
+            xpath = true;
+            comandos.erase(0, 2 + pos);
+            cout << "PATH ES " << comandos.substr(0, comandos.find(" ")) << endl;
+            pos = comandos.find(" ");
+            comandos.erase(0, 1 + pos);
+        } else if (ncomando == "$name" && xname == false) {
+            xname = true;
+            comandos.erase(0, 2 + pos);
+            cout << "Name ES " << comandos.substr(0, comandos.find(" ")) << endl;
+            pos = comandos.find("\n");
+            comandos.erase(0, 1 + pos);
+
+        }
+    }
+
+    cout<<"Al terminar tenermos "<<comandos<<endl;
+
+    /*while (token != NULL) {
         string comando="";
         comando += token;
 
-        /*string ncomando = comando.substr(0, comando.find("=>"));
+        string ncomando = comando.substr(0, comando.find("=>"));
         transform(ncomando.begin(), ncomando.end(), ncomando.begin(), ::tolower);
 
         int pos = comando.find("=>");
@@ -63,9 +103,9 @@ void comando_MKDISK(char *token){
         }else if(ncomando == "$name" && xname == false){
             comando.erase(0, 2+pos);
             cout<<"Name ES "<< comando.substr(0, comando.find("=>"))<<endl;
-        }*/
+        }
         cout<<"COMANDO "<<token<<endl;
 
         token = strtok(NULL, " ");
-    }
+    }*/
 }
