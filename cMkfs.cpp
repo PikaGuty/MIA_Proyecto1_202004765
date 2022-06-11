a07f2ce17994b00c40d91a0e33a2cbd9f97e1992
PikaGuty <3024021520101@ingenieria.usac.edu.gt>
2022-06-11 01:38:53 -0600

    Empezando MKFS

--- cMkfs.cpp
+++ cMkfs.cpp
@@ -3,13 +3,24 @@
 //
 #include <iostream>
 #include "cMkfs.h"
+#include <string.h>
 
 using namespace std;
 
-void cMkfs(int add, char id[16], char unit[16], char type[16]){
+void cMkfs(int add, char id[16], char unitt[16], char type[16]){
     cout<<"Del otro lado"<<endl;
     cout<<"Id: "<<id<<endl;
     cout<<"Unit: "<<unit<<endl;
     cout<<"Type: "<<type<<endl;
     cout<<"Add: "<<add<<endl;
+
+    char *unit;
+    strcpy(unit,unitt);
+
+    if (unit == ' ')//por si no viene unidad
+        unit = 'k';
+    int retorno = 1;
+    retorno = strncmp(type, "", 16);
+    if (retorno == 0)
+        strcpy(type, "full");
 }
